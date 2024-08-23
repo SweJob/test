419ab0a232e42a0f29bbd8aab891d30e80b8d752
SweJob <31406955+SweJob@users.noreply.github.com>
2024-08-23 14:54:46 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,16 +3,16 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
         sprites.destroy(mySprite, effects.fire, 500)
         mp.changePlayerStateBy(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 1)
         if (mp.getPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score) == 1) {
-            mySprite = sprites.create(assets.image`myImage4`, SpriteKind.Food)
+            mySprite = sprites.create(assets.image`myImage6`, SpriteKind.Food)
             mySprite.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
         } else if (mp.getPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score) == 2) {
-            mySprite = sprites.create(assets.image`myImage`, SpriteKind.Food)
+            mySprite = sprites.create(assets.image`myImage7`, SpriteKind.Food)
             mySprite.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
         } else if (mp.getPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score) == 3) {
-            mySprite = sprites.create(assets.image`myImage0`, SpriteKind.Food)
+            mySprite = sprites.create(assets.image`myImage`, SpriteKind.Food)
             mySprite.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
         } else if (mp.getPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score) == 4) {
-            mySprite = sprites.create(assets.image`myImage1`, SpriteKind.Food)
+            mySprite = sprites.create(assets.image`myImage8`, SpriteKind.Food)
             mySprite.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
         } else {
             game.gameOver(true)

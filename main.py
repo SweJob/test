64b820f8b552d0a12bc2df3b3f5a10a302ac66c6
thaYayo <153025998+thaYayo@users.noreply.github.com>
2024-08-23 14:03:26 +0200

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-mp.set_player_sprite(mp.player_selector(mp.PlayerNumber.ONE),
-    sprites.create(assets.image("""
-        Player1
-    """), SpriteKind.player))
 mp.set_player_state(mp.player_selector(mp.PlayerNumber.ONE),
     MultiplayerState.score,
     0)
--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
 mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
-let mySprite2 = sprites.create(assets.image`testsprite`, SpriteKind.Player)
+let mySprite2 = sprites.create(assets.image`
+    testsprite
+`, SpriteKind.Player)
 controller.moveSprite(mySprite2, 100, 100)

ebdb21aded46a14dde51603014d1e6ddafe752a9
SweJob <31406955+SweJob@users.noreply.github.com>
2024-08-23 14:16:53 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     if (mySprite2.overlapsWith(mySprite)) {
-        sprites.destroy(mySprite)
+        sprites.destroy(mySprite, effects.fire, 500)
+        mp.changePlayerStateBy(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 1)
+        if (mp.getPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score) == 1) {
+            mySprite = sprites.create(assets.image`myImage3`, SpriteKind.Food)
+            mySprite.setPosition(randint(0, 100), randint(0, 100))
+        }
     }
 })
 let mySprite2: Sprite = null

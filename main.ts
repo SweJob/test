da2c52cb03c14f1279bf91da7d231d19b220e3f9
SweJob <31406955+SweJob@users.noreply.github.com>
2024-08-23 14:04:13 +0200

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
-let mySprite2 = sprites.create(assets.image`
-    testsprite
-`, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`myImage3`, SpriteKind.Food)
+let mySprite2 = sprites.create(assets.image`testsprite`, SpriteKind.Player)
 controller.moveSprite(mySprite2, 100, 100)

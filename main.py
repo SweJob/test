a1e92a59f5b3a59d5b2073f2494a0f05125bb7b0
thaYayo <153025998+thaYayo@users.noreply.github.com>
2024-08-23 14:00:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,4 @@ mp.set_player_state(mp.player_selector(mp.PlayerNumber.ONE),
 mySprite2 = sprites.create(assets.image("""
     testsprite
 """), SpriteKind.player)
-tiles.set_current_tilemap(tilemap("""
-    bana
-"""))
-tiles.place_on_tile(mySprite2, tiles.get_tile_location(20, 20))
-while not (controller.menu.is_pressed()):
-    scene.camera_follow_sprite(mySprite2)
-    controller.move_sprite(mySprite2, 100, 100)
+controller.move_sprite(mySprite2, 100, 100)

67a1ea4ecdc5fc3a218285f11a635d001089f78a
SweJob <31406955+SweJob@users.noreply.github.com>
2024-08-23 12:12:19 +0200

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,1 @@
-tiles.setCurrentTilemap(tilemap`bana`)
-let Player1 = game.askForString("", 8)
-let Player2 = game.askForString("", 8)
-let Player3 = game.askForString("", 8)
-for (let index = 0; index < 3; index++) {
-    for (let value of Player1) {
-    	
-    }
-}
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Player1`, SpriteKind.Player))

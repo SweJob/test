7095efa947be749e41b075979898f3542d1732ed
SweJob <31406955+SweJob@users.noreply.github.com>
2024-08-23 13:11:04 +0200

    Update main.blocks, tilemap.g.jres, tilemap.g.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# ...
+mp.set_player_sprite(mp.player_selector(mp.PlayerNumber.ONE),
+    sprites.create(assets.image("""
+        Player1
+    """), SpriteKind.player))
+mp.set_player_state(mp.player_selector(mp.PlayerNumber.ONE),
+    MultiplayerState.score,
+    0)
+mySprite2 = sprites.create(assets.image("""
+    testsprite
+"""), SpriteKind.player)
+tiles.set_current_tilemap(tilemap("""
+    bana
+"""))
+tiles.place_on_tile(mySprite2, tiles.get_tile_location(20, 20))
+while not (controller.menu.is_pressed()):
+    scene.camera_follow_sprite(mySprite2)
+    controller.move_sprite(mySprite2, 100, 100)

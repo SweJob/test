572fd9b75e4f31c0e4e281402b4322b26973a003
SweJob <31406955+SweJob@users.noreply.github.com>
2024-08-23 13:58:05 +0200

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,4 @@
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Player1`, SpriteKind.Player))
 mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
 let mySprite2 = sprites.create(assets.image`testsprite`, SpriteKind.Player)
-tiles.setCurrentTilemap(tilemap`bana`)
-tiles.placeOnTile(mySprite2, tiles.getTileLocation(20, 20))
-while (!(controller.menu.isPressed())) {
-    scene.cameraFollowSprite(mySprite2)
-    controller.moveSprite(mySprite2, 100, 100)
-}
+controller.moveSprite(mySprite2, 100, 100)

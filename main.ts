df4a73b3df1d4a64b68a5f1123418766fbd3e08d
SweJob <31406955+SweJob@users.noreply.github.com>
2024-08-23 12:46:32 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Player1`, SpriteKind.Player))
+mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
+while (!(controller.A.isPressed())) {
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One), 100, 100)
+}

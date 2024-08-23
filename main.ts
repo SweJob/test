9129bbdfc913f9c82a3d2b239135e4a8ffc67965
thaYayo <153025998+thaYayo@users.noreply.github.com>
2024-08-23 13:56:47 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Player1`, SpriteKind.Player))
 mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
 let mySprite2 = sprites.create(assets.image`testsprite`, SpriteKind.Player)
-mySprite2.setPosition(0, 0)
-scene.setBackgroundColor(15)
+tiles.setCurrentTilemap(tilemap`bana`)
+tiles.placeOnTile(mySprite2, tiles.getTileLocation(20, 20))
 while (!(controller.menu.isPressed())) {
     scene.cameraFollowSprite(mySprite2)
     controller.moveSprite(mySprite2, 100, 100)

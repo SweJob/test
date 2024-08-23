e7b83b498250899b45f0966d134e54f6f88bd1ca
thaYayo <153025998+thaYayo@users.noreply.github.com>
2024-08-23 14:24:05 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
     if (mySprite2.overlapsWith(mySprite)) {
-        sprites.destroy(mySprite, effects.fire, 500)
-        mp.changePlayerStateBy(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 1)
-        if (mp.getPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score) == 1) {
-            mySprite = sprites.create(assets.image`myImage3`, SpriteKind.Food)
-            mySprite.setPosition(randint(0, 100), randint(0, 100))
-        }
+        sprites.destroy(mySprite)
     }
+    
 })
-let mySprite2: Sprite = null
-let mySprite: Sprite = null
+let mySprite2 : Sprite = null
+let mySprite : Sprite = null
 mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
-mySprite = sprites.create(assets.image`myImage3`, SpriteKind.Food)
-mySprite2 = sprites.create(assets.image`testsprite`, SpriteKind.Player)
+mySprite = sprites.create(assets.image`
+    myImage3
+`, SpriteKind.Food)
+mySprite2 = sprites.create(assets.image`
+    testsprite
+`, SpriteKind.Player)
 mySprite.setPosition(17, 18)
 controller.moveSprite(mySprite2, 100, 100)

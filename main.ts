8474ac184a6bef98c364776f11cd1a7b07e8c697
piskfinne <viklundjonas87@gmail.com>
2024-08-23 11:43:52 +0200

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,9 @@
 tiles.setCurrentTilemap(tilemap`bana`)
-let Players = game.askForString("", 8)
+let Player1 = game.askForString("", 8)
+let Player2 = game.askForString("", 8)
+let Player3 = game.askForString("", 8)
+for (let index = 0; index < 3; index++) {
+    for (let value of Player1) {
+    	
+    }
+}

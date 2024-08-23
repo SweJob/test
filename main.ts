c6059e71282ac652e0f4110d91198ef7df326e05
SweJob <31406955+SweJob@users.noreply.github.com>
2024-08-23 13:59:16 +0200

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Player1`, SpriteKind.Player))
 mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
 let mySprite2 = sprites.create(assets.image`testsprite`, SpriteKind.Player)
 controller.moveSprite(mySprite2, 100, 100)

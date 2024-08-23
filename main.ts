58b9bf77a51fc3707fe2da3f88bf073e4eb60989
SweJob <31406955+SweJob@users.noreply.github.com>
2024-08-23 14:05:44 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
 let mySprite = sprites.create(assets.image`myImage3`, SpriteKind.Food)
 let mySprite2 = sprites.create(assets.image`testsprite`, SpriteKind.Player)
+mySprite.setPosition(17, 18)
 controller.moveSprite(mySprite2, 100, 100)
